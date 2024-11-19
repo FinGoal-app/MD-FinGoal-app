e1c510a5aa3f91611386f537bdab4b7782c4211f
andikapgst <andika.pgst20@gmail.com>
2024-11-19 13:36:45 +0700

    Code updates

--- app/src/main/java/com/bangkit/fingoal/view/goals/GoalsFragment.kt
+++ app/src/main/java/com/bangkit/fingoal/view/goals/GoalsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bangkit.fingoal.databinding.FragmentNotificationsBinding
+import com.bangkit.fingoal.databinding.FragmentGoalsBinding
 
 class GoalsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentGoalsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class GoalsFragment : Fragment() {
         val goalsViewModel =
             ViewModelProvider(this).get(GoalsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentGoalsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
